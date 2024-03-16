8a4ef1b98ff277db9a7670fd67a569ee5f4d4bb7
Dheolarh <adejuwonlookanlawon@gmail.com>
2024-03-16 20:51:49 +0100

    Interface

--- FUTA/chat/static/js/scripts.js
+++ FUTA/chat/static/js/scripts.js
@@ -1,20 +1,20 @@
 function videoControl(videoId) {
     var videos = document.querySelectorAll('.postvid'); // Get all video elements
   
-    videos.forEach(function(video) {
-      video.pause();
-    });
-  
+    // Find the targeted video
     var video = document.getElementById("video" + videoId);
-    video.play();
+  
+    // Toggle playback based on the video's current state
+    video.paused ? video.play() : video.pause();
   }
   
   window.onload = function () {
+    // Bind event handlers to all reaction icons
     var videos = document.querySelectorAll('.reaction-icon');
   
     videos.forEach(function (video, index) {
       video.addEventListener('click', function () {
-        var videoId = index + 1;
+        var videoId = index + 1; // Assuming video IDs start from 1 (adjust if needed)
         videoControl(videoId);
       });
     });
