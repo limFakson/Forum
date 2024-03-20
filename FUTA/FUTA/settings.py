ae7b6a8f246f10423899e71b902147c92cd0ed2f
limfakson <fakeyejoshua2005@gmail.com>
2024-03-20 23:48:16 +0100

    created a new branch

--- FUTA/FUTA/settings.py
+++ FUTA/FUTA/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static')),
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'chat/static')),
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
--- FUTA/chat/urls.py
+++ FUTA/chat/urls.py
@@ -4,12 +4,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.home ,name='Homepage'),
-    path('createAccount', views.createAccount, name= 'createAccount'),
+    path('', views.home, name='Homepage'),
+    path('home', views.home, name='Homepage'),
+    path('forum/register', views.createAccount, name= 'createAccount'),
     path('create_profile', views.create_profile, name= 'create_profile'),
-    path('login', views.login, name= 'Login'),
+    path('forum/login', views.login, name= 'Login'),
     path('logout', views.logout, name = 'logout'),
-    path('profile', views.profile, name= 'profilePage'),
+    path('forum/profile', views.profile, name= 'profilePage'),
 ]
 
 if settings.DEBUG:
