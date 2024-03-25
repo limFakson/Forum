a5e25bda2eac6de5ab8eea7d3f304e75e61dd97c
limfakson <fakeyejoshua2005@gmail.com>
2024-03-25 17:06:00 +0100

    starting new comit

--- FUTA/chat/urls.py
+++ FUTA/chat/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('', views.home, name='Homepage'),
     path('home', views.home, name='Homepage'),
     path('forum/register', views.createAccount, name= 'register'),
-    path('forum/profile/create', views.create_profile, name= 'create_profile'),
+    path('forum/profile/onboarding', views.create_profile, name= 'create_profile'),
     path('forum/logout', views.logout_view, name = 'logout'),
     path('forum/profile', views.profile, name= 'profilePage'),
 ]
