bda85d23607ab149be90e0c76c9de3533cd4485f
limfakson <fakeyejoshua2005@gmail.com>
2024-03-29 01:58:41 +0100

    styling my navbar

--- FUTA/chat/static/js/scripts.js
+++ FUTA/chat/static/js/scripts.js
@@ -1,4 +1,20 @@
+// Script for homepage mobile menu slides
+const openMenu = document.getElementsByClassName('open-menu')[0];
+const closeMenu = document.getElementsByClassName('close-menu')[0];
 
+openMenu.addEventListener('click', () =>{
+    const nav = document.getElementsByClassName('side_nav')[0];
+    console.log('clicked')
+    nav.style.left = '0';
+});
+closeMenu.addEventListener('click', () =>{
+    const nav = document.getElementsByClassName('side_nav')[0];
+    console.log('clicked')
+    nav.style.left = '-100%';
+});
+
+
+// Function for change of profile picture and upload of profile picture
 function uploadImage() {
     document.getElementById('id_profile_picture').click();
 }
@@ -12,8 +28,9 @@ document.getElementById('id_profile_picture').addEventListener('change', functio
       }
       reader.readAsDataURL(file);
     }
-  });
+});
 
+// Script nd Function for form submission and password validation
 const form = document.getElementById('form');
 const error = document.getElementById('error');
 
