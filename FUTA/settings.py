23e33d2c851d4a06f11f2382b0bf1a03aef99308
limfakson <fakeyejoshua2005@gmail.com>
2024-04-15 23:48:28 +0100

    Futa Forum

--- FUTA/settings.py
+++ FUTA/settings.py
@@ -146,6 +146,7 @@ STATICFILES_DIRS = (
 )
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'chat/static/media')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'chat/static')
 
 
 # Default primary key field type
