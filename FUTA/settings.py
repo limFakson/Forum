9733cd14618a0c2855d11d4c92697307ad73a9ae
limfakson <fakeyejoshua2005@gmail.com>
2024-04-16 16:36:42 +0100

    static files

--- FUTA/settings.py
+++ FUTA/settings.py
@@ -136,17 +136,17 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
+STATIC_URL = 'static/'
+MEDIA_URL = 'media/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'chat/static/'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'chat/static/media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media')
 
 # Define STATIC_ROOT to specify where Django should collect static files.
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build', 'chat/static')
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build', 'static')
 
 
 # Default primary key field type
